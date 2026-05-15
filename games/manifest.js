a21be21423dc854fae896c098e952f739a11ec6f
Nicole Selig <nlselig@sep.com>
2026-05-15 14:28:31 -0400

    Revert "clear out test games"
    This reverts commit 0b4d299c1275461cc99223fc72d4449d40cf2252.

--- games/manifest.js
+++ games/manifest.js
@@ -1,2 +1,66 @@
 const GAMES = [
+  {
+    "title": "Rip Tide",
+    "author": "Nicole Selig",
+    "path": "games/nicole-selig/index.html"
+  },
+  {
+    "title": "Spooky platformer",
+    "author": "lee",
+    "path": "games/lee/index.html"
+  },
+  {
+    "title": "Knights of the Round Table",
+    "author": "nicki",
+    "path": "games/nicki/index.html"
+  },
+  {
+    "title": "My Asteroid Game",
+    "author": "Kelly",
+    "path": "games/kelly/index.html",
+    "genre": "Asteroid Field",
+    "color": "neon-cyber"
+  },
+  {
+    "title": "Starry Space",
+    "author": "Nora",
+    "path": "games/nora/index.html",
+    "genre": "Space Shooter",
+    "color": "space"
+  },
+  {
+    "title": "Cyber Blue",
+    "author": "cyber biue",
+    "path": "games/cyber-biue/index.html",
+    "genre": "Platformer",
+    "color": "neon-cyber"
+  },
+  {
+    "title": "test",
+    "author": "test",
+    "path": "games/test/index.html",
+    "genre": "Platformer",
+    "color": "ocean"
+  },
+  {
+    "title": "my test agent game",
+    "author": "test-agent",
+    "path": "games/test-agent/index.html",
+    "genre": "Platformer",
+    "color": "neon-cyber"
+  },
+  {
+    "title": "Snake",
+    "author": "Jean",
+    "path": "games/jean/index.html",
+    "genre": "Snake 3D",
+    "color": "sunset"
+  },
+  {
+    "title": "Cyber Cool",
+    "author": "coolest",
+    "path": "games/coolest/index.html",
+    "genre": "Platformer",
+    "color": "neon-cyber"
+  }
 ];
