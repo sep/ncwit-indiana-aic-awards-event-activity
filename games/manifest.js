f8304f7db91d91df99d42eb720774c0f51468299
Nicole Selig <nicoleleeselig@gmail.com>
2026-05-16 10:02:27 -0400

    thanishka's game

--- games/manifest.js
+++ games/manifest.js
@@ -5,5 +5,12 @@ const GAMES = [
     "path": "games/nicole/index.html",
     "genre": "Brick Breaker",
     "color": "Neon Cyber"
+  },
+  {
+    "title": "City Runner",
+    "author": "Thanishka",
+    "path": "games/thanishka/index.html",
+    "genre": "Maze Runner",
+    "color": "Sunset"
   }
 ];
